536051682f2d7f60b9d04d48c591dcf0e078c3bc
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-12-12 15:00:42 +0530

    setTitle & setContent

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, UseEffect } from 'react'
 import './Home.css';
 
-import Navbar from '../../components/Navbar/Navbar';
 import Note from '../../components/Note/Note';
 
 function Home() {
@@ -13,33 +12,65 @@ function Home() {
     {
       title: "Note 2",
       content: "This is content of note 2"
-    },
-    {
-      title: "Note 3",
-      content: "This is content of note 3"
-    },
-    {
-      title: "Note 4",
-      content: "This is content of note 4"
     }
+
   ])
+
+  const [title,setTitle]= useState("")
+  const [content,setContent]= useState("")
+
+  function addNote(){
+    const newNote = {
+      "title":title,
+      "content":content
+    }
+    setNotes([...notes,newNote])
+
+    setTitle("")
+    setContent("")
+
+
+  }
+
 
   return (
     <div>
-      <Navbar />
       <div className='app-title-container'>
         <h1 className='app-title'>My Notes</h1>
       </div>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='notes-container'>
+            {
+              notes.map((note, index) => {
+                return (
+                  <Note title={note.title} content={note.content} />
+                )
+              })
+            }
+          </div>
 
-      <div className='notes-container'>
-        {
-          notes.map((note, index) => {
-            return (
-              <Note title={note.title} content={note.content}/>
-            )
-          })
-        }
+        </div>
+
+        <div className='col-md-6'>
+          <div className='note-editor-container'>
+            <h3 className='text-center'>Add Note</h3>
+            <form>
+              <div>
+                <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className='form-control mt-4 ' placeholder='Note Title' />
+                <input type="text"  value={content} onChange={(e)=>{setContent(e.target.value)}}className='form-control mt-4 ' placeholder='Note description' />
+              </div>
+              <div className='add-note-button-container'>
+              <button  type="button"className='btn btn-info mt-4' onClick={addNote}>Add Note</button>
+              </div>
+            </form>
+
+          </div>
+
+        </div>
       </div>
+
+
     </div>
 
   );
